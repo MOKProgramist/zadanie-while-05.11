4785975f9a3e6d99f909f34111a408dd7678f714
MOKProgramist <is24romanovar@artcollege.ru>
2021-11-05 01:06:44 +0300

    Добавьте файлы проекта.

---
+++ 4/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace _4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool otvet = true;
+            string test; // переменная для сравнения ответа
+            while (otvet) // работает пока переменная равна true 
+            {
+                int perv, posl, a, b, c, d; // переменные
+                Console.Write("Введите число: ");
+                int n = int.Parse(Console.ReadLine());
+
+                posl = n % 10; // полседняя цифра
+                d = n;
+                c = n;
+                b = n;
+                a = 1;
+                perv = 0;
+
+                if (b < 0) // условие
+                {
+                    Console.WriteLine("Число не может быть меньше 0!");
+                    return;
+                }
+
+                while ( b > 0) // пока b > 0, выполняем
+                {
+                    perv = b % 10;
+                    a = a * 10;
+                    b /= 10;
+                }
+                b = c - perv * (a / 10) + posl * (a / 10) - posl + perv;
+                Console.WriteLine($"Изменил последнюю и первую цифру числа \"{d}\" = \"{b}\"");
+                Console.WriteLine("Если хочешь продолжить введи - true (t), если нет - false (f)");
+                // часть кода для повтора кода
+                test = Console.ReadLine(); // считываем текст
+                otvet = ((test == "true" || test == "t") ? true : false); // сравниваем ответ с true, если совпадает не изменяем и повторяем действие с преобразованием, если нет, то заканчиваем
+            }
+        }
+    }
+}
---
+++ zadanie-dop-while/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace zadanie_dop_while
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try // обрабатываем ошибки // но пока это не очень работает(
+            {
+                Console.Write("Введите число: ");
+                int a = int.Parse(Console.ReadLine()); // считываем консоль и объявляем переменную
+                int s = 0;
+
+                Console.Write($"Сумма числел числа \"{a}\" = "); // первый вывод сообщения, чтобы не добавлять новую переменную
+                while (a > 0) // пока a будет > 0, цикл будет работать
+                {
+
+                    s = s + a % 10; // раскладываем число на числа :)
+                    a = a / 10; // сокращаем количество итераций цикла
+
+                }
+                Console.Write($"{s} "); // окончательный резульат
+
+                // Console.WriteLine($"Сумма числел числа \"{a}\" = {s}"); // 
+                Console.ReadKey();
+            } catch (ExecutionEngineException ex)
+            {
+                Console.WriteLine(ex);
+                Console.ReadKey();
+            }
+        }
+    }
+}
